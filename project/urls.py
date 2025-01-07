eebdbaf1e894736a91417727c14fd36d668e9e2c
OmarBakr-93 <omarbakrabdallah.181993@gmail.com>
2025-01-07 15:33:54 +0200

    finish the full project

---
+++ project/urls.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
+
+urlpatterns = [
+    path('', include('pages.urls',namespace="pages")),
+    path('listings/', include('listings.urls',namespace="listings")),
+    path('accounts/', include('accounts.urls',namespace="accounts")),
+    path('contacts/', include('contacts.urls', namespace="contacts")),
+    path('admin/', admin.site.urls),
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
